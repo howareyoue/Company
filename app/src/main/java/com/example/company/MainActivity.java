b7ace0eab9cc3f9f02feb037c856eae4f461fd0e
uijin23 <0305uijin@naver.com>
2023-12-14 04:06:06 +0900

    로그아웃 버튼추가

--- app/src/main/java/com/example/company/MainActivity.java
+++ app/src/main/java/com/example/company/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button hob;
@@ -14,10 +16,18 @@ public class MainActivity extends AppCompatActivity {
     private Button taste;
     private Button talk;
 
+    private Button logoutButton;
+
+    private FirebaseAuth mFirebaseAuth; // Firebase 인증 객체
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance(); // Firebase 인증 객체 초기화
+
 
         hob = findViewById(R.id.btn_hobby);
         hob.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +64,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        logoutButton = findViewById(R.id.btn_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Firebase 로그아웃
+                mFirebaseAuth.signOut();
 
-
+                // 로그인 페이지로 이동
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(intent);
+                finish(); // 현재 액티비티를 종료하여 뒤로가기 시 로그인 페이지가 나오도록 함
+            }
+        });
     }
 }
+
+
